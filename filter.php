c5bcf75c127f0d357a355471a26378a0cb267c3b
Andrea Battellocchi <yellowelise@gmail.com>
2016-04-01 14:18:25 +0200

    Added files via upload

---
+++ filter.php
@@ -0,0 +1,41 @@
+<?php
+ini_set('memory_limit', '32048M');
+include("class/im.class.php");
+
+$w = 800;
+$h = 600;
+$c = 0;
+$s=0;
+$i=0;
+$arg1=128;
+$arg2=128;
+$arg3=128;
+$arg4=128;
+/*end default*/
+
+/*override if passed from $_REQUEST*/
+
+foreach ($_REQUEST as $k=>$v)
+ ${$k} = $v;
+
+$im = new ImageManipulate();
+$q=0;
+foreach (glob("{media".DIRECTORY_SEPARATOR."*.jpg,media".DIRECTORY_SEPARATOR."*.jpeg,media".DIRECTORY_SEPARATOR."*.gif,media".DIRECTORY_SEPARATOR."*.png}",GLOB_BRACE) as $filename) 
+ {
+	 ${"filename".$q} = $filename;
+	 $q++;
+ }
+
+
+  $ra = rand(0,$q-1);
+  $filename = ${"filename".$ra};
+  $ext = strtolower(substr(strrchr($filename, '.'), 1));  
+  header('Content-Type: ' . $im->extension_to_image_type($ext));
+
+  $im->CachePath = (__DIR__).DIRECTORY_SEPARATOR . "im_cache".DIRECTORY_SEPARATOR;
+
+  	
+  $im->ApplyFilter($filename,$i,$w,$h,$c,$s,$arg1,$arg2,$arg3,$arg4);
+
+?>
+
---
+++ wall.php
@@ -0,0 +1,45 @@
+<?php
+ini_set('memory_limit', '32048M');
+include("class/im.class.php");
+
+
+/* default value */
+
+$w = 800;
+$h = 600;
+$r = 0;
+$c = 0;
+/*end default*/
+
+/*override if passed from $_REQUEST*/
+
+foreach ($_REQUEST as $k=>$v)
+ ${$k} = $v;
+
+
+$im = new ImageManipulate();
+
+
+
+$q=0;
+foreach (glob("{media".DIRECTORY_SEPARATOR."*.jpg,media".DIRECTORY_SEPARATOR."*.jpeg,media".DIRECTORY_SEPARATOR."*.gif,media".DIRECTORY_SEPARATOR."*.png}",GLOB_BRACE) as $filename) 
+ {
+	 ${"filenamejpg".$q} = $filename;
+	 $q++;
+ }
+
+
+for ($i=0;$i<rand(4,55);$i++)
+ {
+  $ra = rand(0,$q-1);
+  $fna[] = array("filename"=>${"filenamejpg".$ra});
+ } 
+
+header('Content-Type: ' . $im->extension_to_image_type("jpg"));
+
+$im->CachePath = (__DIR__).DIRECTORY_SEPARATOR . "im_cache".DIRECTORY_SEPARATOR;
+ 
+$im->append_array($fna,$w,$h,$c,$r);
+
+?>
+
